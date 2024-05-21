ead91412d06cf66671a41e6cf08db9eecd827c86
theboss17121999 <46495612+theboss17121999@users.noreply.github.com>
2024-05-21 14:01:48 +0530

    minor changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,32 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export const Contact = () => {
     return (
-        <>
-            <div className="h-full md:h-screen bg-purple-500">
-                <LetsTalk />
-            </div>
-        </>
-    )
+        <div className="bg-purple-500 ">
+            <LetsTalk />
+        </div>
+    );
 }
-
 
 function LetsTalk() {
     return (
-        <div className="flex justify-center w-full py-20 sc:px-10 h-full">
-            <div className="w-full sc:w-170 flex flex-col justify-between h-full">
-                <div>
-                    <div className="text-7xl md:text-scextralarge sc:text-extralarge text-white pt-4 md:pt-32 flex justify-center sc:justify-start">
-                        Let's Talk
-                    </div>
-                    <div className="text-2xl md:text-5xl text-white pt-8 flex justify-center sc:justify-start">
-                        Feel Free to Contact
-                    </div>
+        <div className="w-full p-8 md:p-16 bg-purple-700 rounded-lg shadow-lg">
+            <div className="flex flex-col content-center items-center ">
+                <div className="text-5xl md:text-7xl lg:text-9xl text-white font-bold pt-4 md:pt-8">
+                    Let's Talk
                 </div>
-                
-                <div className="text-2xl md:text-5xl text-white pt-8 md:pt-32 flex justify-center sc:justify-start mt-auto">
-                    <button onClick={() => window.location = 'mailto:shashwatbose1712@gmail.com'} className="underline underline-offset-8 hover:text-black transition duration-500">
+                <div className="text-xl md:text-2xl lg:text-3xl text-white pt-4 md:pt-8">
+                    Feel Free to Contact
+                </div>
+                <div className="text-lg md:text-xl lg:text-2xl text-white pt-4 md:pt-16 flex flex-col items-center ">
+                    <button 
+                        onClick={() => window.location = 'mailto:shashwatbose1712@gmail.com'} 
+                        className="underline underline-offset-8 hover:text-black transition duration-500 mt-4"
+                    >
                         shashwatbose1712@gmail.com
                     </button>
                 </div>
@@ -34,3 +30,5 @@ function LetsTalk() {
         </div>
     );
 }
+
+export default Contact;
