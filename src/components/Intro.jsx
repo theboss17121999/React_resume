679d3a8994f06d342989bdd5e2e784a60b8c6cbe
theboss17121999 <46495612+theboss17121999@users.noreply.github.com>
2024-06-01 16:08:39 +0530

    Minor Changes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,6 @@
-import C1 from '../assets/Intro/Cloud1.jpg';
-import C2 from '../assets/Intro/cloud2.jpg';
-import P1 from '../assets/Intro/pond.jpg';
-import sv1 from '../assets/background/tiles1.svg'
 import sv2 from '../assets/background/low-poly-grid-haikei.svg'
-import sv3 from '../assets/background/wavey-fingerprint.svg'
 import {HashLink as Link} from "react-router-hash-link"
+import { TypeAnimation } from 'react-type-animation';
 
 
 export const Intro = ({ title }) => {
@@ -14,7 +10,40 @@ export const Intro = ({ title }) => {
             <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
                 <div className="font-display2 text-4xl text-whitw mb-4">Hi, My name is</div>
                 <div className="font-handwriting text-6xl md:text-8xl text-purple-700 mb-8">{title}.</div>
-                <div className="font-display2 text-3xl md:text-6xl text-white mb-6 flex justify-content-center">I make web applications</div>
+                <div className="font-handwriting text-3xl md:text-6xl text-white mb-6 inline-block">
+                    <TypeAnimation
+                        sequence={[
+                            'I am a Developer.',
+                            2000,
+                            'I make web applications.',
+                            2000,
+                            'I create Java applications.',
+                            2000,
+                            'I have expertise in Data Structures and Algorithms.',
+                            2000,
+                            'I specialize in Tailwind CSS and responsive design.',
+                            2000,
+                            'I solve complex problems.',
+                            2000,
+                            'I am passionate about clean code.',
+                            2000,
+                            'I enjoy learning new technologies.',
+                            2000,
+                            'I work well in teams.',
+                            2000,
+                            'I value clear communication.',
+                            2000,
+                            'I am always learning new skills.',
+                            2000,
+                            'I stay updated with industry trends.',
+                            2000
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        repeat={Infinity}
+                    />
+                </div>
+
                 {/* <div className="font-pixel text-lg text-green leading-relaxed max-w-2xl mx-auto">
                     I am a highly skilled programmer with expertise in the M.E.R.N. Stack. With a passion for solving complex problems and a strong foundation in programming languages, I strive to create efficient and scalable solutions.
                 </div> */}
