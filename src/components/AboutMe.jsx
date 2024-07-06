d8602e6792e6b381df1cd7ba254d7782c718062b
theboss17121999 <46495612+theboss17121999@users.noreply.github.com>
2024-07-06 17:03:45 +0530

    final changes

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -45,73 +45,73 @@ import {
   } from "@radix-ui/react-icons";
 
   const LOGOS1 = [
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <SiMongodb className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">Mongo DB</p>
     </div>,
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <SiExpress className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">Express</p>
     </div>,
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <SiReact className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">React</p>
     </div>,
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <FaNodeJs className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">Node.js</p>
     </div>,
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <SiTailwindcss className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">Tailwind CSS</p>
     </div>
 ];
 
 const LOGOS2 = [
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <FaJava className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">Java</p>
     </div>,
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <SiJavascript className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">JavaScript</p>
     </div>,
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <SiTypescript className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">TypeScript</p>
     </div>,
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <FaPython className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">Python</p>
     </div>
 ];
 
 const LOGOS3 = [
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <FaHtml5 className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">HTML5</p>
     </div>,
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <FaCss3Alt className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">CSS3</p>
     </div>,
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <SiMysql className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">MySQL</p>
     </div>,
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <SiGnometerminal className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">Terminal</p>
     </div>,
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <FaGitAlt className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">Git</p>
     </div>,
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <FaGithub className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">GitHub</p>
     </div>,
-    <div className='slide flex flex-col items-center justify-center w-[125px] mx-4'>
+    <div className='slide flex flex-col items-center justify-center w-[100px] mx-4'>
         <SiPostman className="text-slate-800 w-16 h-16" />
         <p className="text-center mt-2">Postman</p>
     </div>
@@ -263,7 +263,7 @@ function InfiniteSlider1() {
     return (
         <div className="relative w-[500px] overflow-hidden before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,#fafaf9_0%,rgba(250,250,249,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,#fafaf9_0%,rgba(250,250,249,0)_100%)] after:content-['']">
         <div className="animate-infinite-slider flex w-[calc(125px*{LOGOS1.length * 4})]">
-          {[...Array(4)].map((_, i) => (
+          {[...Array(5)].map((_, i) => (
             LOGOS1.map((logo, index) => (
               <div key={`${i}-${index}`}>
                 {logo}
@@ -279,7 +279,7 @@ function InfiniteSlider1() {
     return (
         <div className="relative w-[500px] overflow-hidden before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,#fafaf9_0%,rgba(250,250,249,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,#fafaf9_0%,rgba(250,250,249,0)_100%)] after:content-['']">
         <div className="animate-infinite-slider1 flex w-[calc(125px*{LOGOS2.length * 4})]">
-          {[...Array(5)].map((_, i) => (
+          {[...Array(6)].map((_, i) => (
             LOGOS2.map((logo, index) => (
               <div key={`${i}-${index}`}>
                 {logo}
@@ -295,7 +295,7 @@ function InfiniteSlider1() {
     return (
         <div className="relative w-[500px] overflow-hidden before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,#fafaf9_0%,rgba(250,250,249,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,#fafaf9_0%,rgba(250,250,249,0)_100%)] after:content-['']">
         <div className="animate-infinite-slider flex w-[calc(125px*{LOGOS3.length * 4})]">
-          {[...Array(4)].map((_, i) => (
+          {[...Array(5)].map((_, i) => (
             LOGOS3.map((logo, index) => (
               <div key={`${i}-${index}`}>
                 {logo}
